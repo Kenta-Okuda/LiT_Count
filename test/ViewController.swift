5e9c070e26380e0a21d6b7f07eb56ca427fa0fed
Kenta Okuda <kenta19940520@gmail.com>
2016-06-05 14:43:33 +0900

    first commit

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count : Int = 0
+    @IBOutlet var lavel :UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // DBany additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func plus() {
+        count += 1
+        lavel.text = String(count)
+        checkNum()
+    }
+    
+    @IBAction func minus() {
+        count -= 1
+        lavel.text = String(count)
+        checkNum()
+    }
+    
+    func checkNum() {
+        if( count > 0 ) {
+            lavel.textColor = UIColor.blueColor()
+        } else if( count < 0 ) {
+            lavel.textColor = UIColor.redColor()
+        } else if( count == 0) {
+             lavel.textColor = UIColor.blackColor()
+        }
+    }
 
 }
 
